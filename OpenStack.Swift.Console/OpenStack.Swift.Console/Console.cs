6b38a84a7de77ce287d0519fa58c31efc03af308
ed tsang <edwardt.tril@gmail.com>
2012-08-02 12:06:54 -0700

    Added Console project slowly

---
+++ OpenStack.Swift.Console/OpenStack.Swift.Console/Console.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenStack.Swift.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Console
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ OpenStack.Swift.Console/OpenStack.Swift.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenStack.Swift.Console")]
+[assembly: AssemblyDescription("OpenStack Swift Console Client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenStack.Swift.Console")]
+[assembly: AssemblyCopyright("etsang")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0549859-0f15-4520-ae66-849c7c1ee9dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
